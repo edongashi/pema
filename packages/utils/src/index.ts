432cebc175e6cd2cf21b4f405d629dbe622609fe
Edon Gashi <edon.gashi@uni-pr.edu>
2019-06-08 17:27:29 +0200

    index

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
 export { warning, invariant } from './dev-utils'
 export { serializeError, stringifyError } from './error-utils'
-export { buildProps, toArray, noop } from './misc-utils'
+export { buildProps, toArray, noop, mapLazy } from './misc-utils'
 export * from './types'
