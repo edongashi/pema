045eb18ac9b15a8ec9749046c6df2313d9aa0693
Edon Gashi <edon.gashi@uni-pr.edu>
2019-06-08 19:06:45 +0200

    simplify code

--- packages/router/src/with-router.ts
+++ packages/router/src/with-router.ts
@@ -1,15 +1,13 @@
-import { AppPlugin, AppNode, AppOptions } from '@pema/app'
+import { AppNode, AppOptions } from '@pema/app'
 import { RouterEnv } from './types'
 import Router from './router'
 
 export default function withRouter
   <TApp extends AppNode = AppNode>(options: AppOptions<RouterEnv, TApp>) {
   const plugin = (app: AppNode) => {
-    const result = app.extend({
+    return app.extend({
       router: [Router, options]
     })
-
-    return result
   }
 
   return plugin
