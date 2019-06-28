39d881be0cb457f379c8238394009207f543da7d
Edon Gashi <edon.gashi@uni-pr.edu>
2019-06-28 15:25:27 +0200

    useController

--- packages/router-react/src/hooks.ts
+++ packages/router-react/src/hooks.ts
@@ -1,6 +1,6 @@
 import { AppNode } from '@pema/app'
 import { useApp, useEvent } from '@pema/app-react'
-import { Router, RouterState } from '@pema/router'
+import { Controller, Router, RouterState } from '@pema/router'
 import { invariant } from '@pema/utils'
 
 interface RouterApp extends AppNode {
@@ -17,4 +17,8 @@ export function useCurrentLocation(): RouterState {
   const router = useRouter()
   useEvent('router.current')
   return router.current
+}
+
+export function useController<TController extends Controller>(): TController {
+  return useCurrentLocation().controller as TController
 }
--- packages/router-react/src/index.ts
+++ packages/router-react/src/index.ts
@@ -1,4 +1,4 @@
-export { useRouter } from './hooks'
+export { useRouter, useCurrentLocation, useController } from './hooks'
 export { Link, LinkProps } from './Link'
 export { NavLink, NavLinkProps } from './NavLink'
 export { Prefetch, PrefetchProps } from './Prefetch'
